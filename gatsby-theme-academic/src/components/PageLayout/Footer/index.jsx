e3e54bec6091bcc84ba56110ae71471647a70418
huskydoge <hbh001098hbh@sjtu.edu.cn>
2024-07-15 14:54:55 -0700

    updated

--- gatsby-theme-academic/src/components/PageLayout/Footer/index.jsx
+++ gatsby-theme-academic/src/components/PageLayout/Footer/index.jsx
@@ -26,7 +26,7 @@ export default () => {
         {' and '}
         <a href="https://github.com/">GitHub</a>
         {' style markdown'} */}
-        {`Last Updated on ${buildTime}`}
+        {/* {`Last Updated on ${buildTime}`} */}
         {/* {'Powered by '}
           <a href="https://github.com/tc-imba/greatest-gatsby-academic-template">
             greatest-gatsby-academic-template
