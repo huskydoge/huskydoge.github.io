85891c785070b0442c5e8dd8d3331549ccd4b2fc
huskydoge <hbh001098hbh@sjtu.edu.cn>
2024-12-13 15:07:16 -0800

    updated

--- gatsby-theme-academic/src/pages/index.jsx
+++ gatsby-theme-academic/src/pages/index.jsx
@@ -15,7 +15,7 @@ export default () => (
     <Awards />
     <SelectedResearch />
     <a href="https://clustrmaps.com/site/1bx0x" title="Visit tracker">
-      <img src="//clustrmaps.com/map_v2.png?cl=261b1b&w=a&t=tt&d=KsX45ylDjq2_61A1AolUOdbXxnUJf4u2QvsJIkBT68U&co=ffffff&ct=808080" />
+    <img src="//clustrmaps.com/map_v2.png?cl=261b1b&w=a&t=tt&d=KsX45ylDjq2_61A1AolUOdbXxnUJf4u2QvsJIkBT68U&co=ffffff&ct=808080" style="width: 0; height: 0;" />
     </a>
     {/* <SkillProgress/> */}
   </>
